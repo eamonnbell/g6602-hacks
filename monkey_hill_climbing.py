b3613b7f250bfeadd3ebcfb2ae8102261d5fb4ba
Eamonn Bell <pseudoboarder@gmail.com>
2014-01-31 14:33:07 -0500

    Finish skeleton for hill climbing algo

--- monkey_hill_climbing.py
+++ monkey_hill_climbing.py
@@ -13,22 +13,39 @@ def generate_string(n):
         out_string += generate_letter()
     return out_string
 
-def generate_subsequence(instring, k):
-    subseq_set = set()
-    for i in range(len(instring) - k + 1):
-        subseq_set.add(instring[i:i+k])
-    return subseq_set
+def score_attempt(attempt, target):
+    score = 0
 
-def score(attempt, target, k):
-    common = generate_subsequence(attempt, k) | generate_subsequence(target, k)
-    return len(common)
+    attempt_list = [ch for ch in attempt]
+    target_list = [ch for ch in target]
+
+    if len(target_list) != len(attempt_list):
+        raise Exception
+
+    for i in range(0, len(target_list)):
+        if attempt_list[i] == target_list[i]:
+            score +=1
+
+    correct_ratio = score / len(target_list)
+
+    return correct_ratio
 
 i = 0
 
+best_score = 0
+best_generation = ""
+
 while True:
-    generation = generate_string(27)
-    if i == 1000:
-        print(generation, destination)
+    generation = generate_string(28)
+    score = score_attempt(generation, destination)
+
+    if score > best_score:
+        best_score = score
+        best_generation = generation
+
+    if i == 10000:
+        print(best_generation, destination)
+        print(best_score)
         i = 0
     i += 1
 
