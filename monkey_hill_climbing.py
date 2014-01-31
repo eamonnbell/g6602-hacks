81bb575253d5ddb136ed4f7ea861418dc6735eae
Eamonn Bell <pseudoboarder@gmail.com>
2014-01-31 15:06:02 -0500

    Finish hill climbing. Still a bug.

--- monkey_hill_climbing.py
+++ monkey_hill_climbing.py
@@ -1,6 +1,6 @@
 import random
 
-destination = "methinks it is like a weasel"
+destination = "methinks it is like a weasel methinks it is like a weasel"
 
 alphabet = "abcdefghijklmnopqrstuvwxyz "
 
@@ -13,11 +13,25 @@ def generate_string(n):
         out_string += generate_letter()
     return out_string
 
+def find_index_of_errors(attempt, target):
+    position_list = []
+
+    attempt_list = list(attempt)
+    target_list = list(target)
+
+    if len(target_list) != len(attempt_list):
+        raise Exception
+
+    for i in range(0, len(target_list)):
+        if attempt_list[i] != target_list[i]:
+            position_list.append(i)
+    return position_list
+
 def score_attempt(attempt, target):
     score = 0
 
-    attempt_list = [ch for ch in attempt]
-    target_list = [ch for ch in target]
+    attempt_list = list(attempt)
+    target_list = list(target)
 
     if len(target_list) != len(attempt_list):
         raise Exception
@@ -30,22 +44,22 @@ def score_attempt(attempt, target):
 
     return correct_ratio
 
-i = 0
+def mutate(instring, position_list):
+    instring_list = list(instring)
+    char_to_mutate = position_list[random.randint(0,len(position_list) - 1)]
+    instring_list[char_to_mutate] = generate_letter()
+    return "".join(instring_list)
 
+i = 0
+seed = generate_string(len(destination))
+generation = seed
 best_score = 0
-best_generation = ""
 
 while True:
-    generation = generate_string(28)
-    score = score_attempt(generation, destination)
-
-    if score > best_score:
-        best_score = score
-        best_generation = generation
-
-    if i == 10000:
-        print(best_generation, destination)
-        print(best_score)
-        i = 0
-    i += 1
+    error_index = find_index_of_errors(generation, destination)
+    mutation = mutate(generation, error_index)
+    print(generation)
+    if score_attempt(mutation, destination) > best_score:
+        generation = mutation
+        best_score = score_attempt(generation, destination)
 
