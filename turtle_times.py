ca9ab98c523e4e49d5c3df88b604541e149f372f
Eamonn Bell <pseudoboarder@gmail.com>
2014-02-05 00:39:27 -0500

    First commit of news-reading turtle.

---
+++ turtle_times.py
@@ -0,0 +1,24 @@
+import turtle
+import fileinput
+import random
+
+wordlist = []
+
+for line in fileinput.input():
+  for word in line.strip().split(" "):
+    wordlist.append(word)
+
+datalist = [(len(word) if len(word) > 3 else 3, word) for word in wordlist]
+
+window = turtle.Screen()
+
+frank = turtle.Turtle()
+frank.speed(10)
+count = 0
+
+for pair in datalist:
+  window.title(pair[1])
+  frank.forward(pair[0]*3)
+  turn_angle = random.choice([90,270])  
+  frank.left(turn_angle)
+
